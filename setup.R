332e0a60efec8e15cdf19fd7217d72597583ef32
Bhavya Singh <singhb39@mcmaster.ca>
2023-05-21 20:05:45 -0400

    heatmaps, volcano plots, plot counts

--- setup.R
+++ setup.R
@@ -24,6 +24,8 @@ library(ggsci)
 library(ggplot2)
 library(ggpubr)
 library(fgsea)
+library(pheatmap)
+library(scales)
 
 ################################## LOAD REFS ###################################
 
@@ -139,6 +141,82 @@ brain.filt.cor.l1 <- brain.cor.l1[rowSums(brain.cor.l1 > cutoff.count) >cutoff.s
 save(brain.filt.cor.tx, brain.filt.cor.rtx, brain.filt.cor.comb, brain.filt.cor.herv, brain.filt.cor.l1,
      brain.samples, file="r_outputs/brain.filt.cor.Rdata")
 
+############################# COUNT READS PER TYPE ############################# 
+
+te_percent <- function(herv.df, rtx.df, comb.df, metadata, metadata.col) {
+  
+  herv.reads <- as.data.frame(colSums(herv.df))
+  te.reads <- as.data.frame(colSums(rtx.df))
+  all.reads <- as.data.frame(colSums(comb.df))
+  
+  colnames(herv.reads) <- c("reads")
+  colnames(te.reads) <- c("reads")
+  colnames(all.reads) <- c("reads")
+  
+  herv.reads$sample  <- rownames(herv.reads)
+  te.reads$sample <- rownames(te.reads)
+  all.reads$sample <- rownames(all.reads)
+  
+  herv.reads$type <- metadata[[metadata.col]]
+  te.reads$type <- metadata[[metadata.col]]
+  all.reads$type <- metadata[[metadata.col]]
+  
+  stopifnot(all(all.reads$sample == te.reads$sample))
+  
+  te.reads$proportion <- te.reads$reads/all.reads$reads*100
+  herv.reads$proportion <- herv.reads$reads/all.reads$reads*100
+  
+  output <- list(herv.reads = herv.reads,
+                 te.reads = te.reads)
+  
+  return(output)
+}
+
+###################### COUNT READS PER AGIRRE TISSUE TYPE ###################### 
+
+brain.te.percent <-
+  te_percent(brain.filt.cor.herv, brain.filt.cor.rtx, brain.filt.cor.comb,
+             brain.samples, "SMTSD")
+
+
+brain.te.percent$herv.reads %>%
+  group_by(type) %>%
+  summarise_at(vars(proportion), list(name = mean))
+
+brain.te.percent$te.reads %>%
+  group_by(type) %>%
+  summarise_at(vars(proportion), list(name = mean))
+
+brain.te.percent$te.reads %>%
+  ggplot(aes(x=type, y=proportion, fill=type))  +
+  geom_boxplot(notch = TRUE) +
+  theme_pubr() +
+  theme(legend.position="none",
+        axis.text.x = element_text(angle=45, hjust=1)) +
+  xlab("Cell Type") +
+  ylab("% of TE Fragments") +
+  ylim(0, 0.75) +
+  scale_fill_manual(values = c("Brain - Frontal Cortex (BA9)" = pal_jco("default", alpha = 0.8)(7)[1],
+                               "Brain - Hippocampus" = pal_jco("default", alpha = 0.8)(7)[7])) + 
+  scale_x_discrete(labels=c("Brain - Frontal Cortex (BA9)" = "FC", 
+                            "Brain - Hippocampus" = "HC")) +
+  theme(aspect.ratio = 1)
+
+brain.te.percent$herv.reads %>%
+  ggplot(aes(x=type, y=proportion, fill=type))  +
+  geom_boxplot(notch = TRUE) +
+  theme_pubr() +
+  theme(legend.position="none",
+        axis.text.x = element_text(angle=45, hjust=1)) +
+  xlab("Cell Type") +
+  ylab("% of HERV Fragments") +
+  ylim(0, 0.75) +
+  scale_fill_manual(values = c("Brain - Frontal Cortex (BA9)" = pal_jco("default", alpha = 0.8)(7)[1],
+                               "Brain - Hippocampus" = pal_jco("default", alpha = 0.8)(7)[7])) + 
+  scale_x_discrete(labels=c("Brain - Frontal Cortex (BA9)" = "FC", 
+                            "Brain - Hippocampus" = "HC")) +
+  theme(aspect.ratio = 1)
+
 ################################# FUNCTION SCREE PLOT #################################
 
 pca_standard <- function(tform, metadata, var) {
@@ -214,8 +292,8 @@ biplot(brain.gh.pca.obj,
        lengthLoadingsArrowsFactor = 1.5,
        drawConnectors = TRUE,
        colby = "SMTSD",
-       colkey = c("Brain - Frontal Cortex (BA9)" = "orange", 
-                  "Brain - Hippocampus" = "lightblue"),
+       colkey = c("Brain - Frontal Cortex (BA9)" = pal_jco("default", alpha = 0.8)(7)[1], 
+                  "Brain - Hippocampus" = pal_jco("default", alpha = 0.8)(7)[7]),
        legendPosition = "right")  +
   theme_cowplot()
 
@@ -251,8 +329,8 @@ biplot(brain.gh.cor.pca.obj,
        lengthLoadingsArrowsFactor = 1.5,
        drawConnectors = TRUE,
        colby = "SMTSD",
-       colkey = c("Brain - Frontal Cortex (BA9)" = "orange", 
-                  "Brain - Hippocampus" = "lightblue"),
+       colkey = c("Brain - Frontal Cortex (BA9)" = pal_jco("default", alpha = 0.8)(7)[1], 
+                  "Brain - Hippocampus" = pal_jco("default", alpha = 0.8)(7)[7]),
        legendPosition = "right")  +
   theme_cowplot()
 
@@ -285,8 +363,8 @@ biplot(brain.herv.cor.pca.obj,
        lengthLoadingsArrowsFactor = 1.5,
        drawConnectors = TRUE,
        colby = "SMTSD",
-       colkey = c("Brain - Frontal Cortex (BA9)" = "orange", 
-                  "Brain - Hippocampus" = "lightblue"),
+       colkey = c("Brain - Frontal Cortex (BA9)" = pal_jco("default", alpha = 0.8)(7)[1], 
+                  "Brain - Hippocampus" = pal_jco("default", alpha = 0.8)(7)[7]),
        legendPosition = "right")  +
   theme_cowplot()
 
@@ -296,4 +374,106 @@ save(brain.herv.cor.dds, brain.herv.cor.pca.obj, file = "r_outputs/brain.herv.co
 
 lfc.cutoff <- 1.5
 pval=0.001 # p value threshold
+
+############################### TOP HERVs ONLY #################################
+
+brain.res.herv <- DESeq2::results(brain.herv.cor.dds, contrast=c("SMTSD", "Brain - Hippocampus", 
+                                         "Brain - Frontal Cortex (BA9)"), alpha=pval)
+
+brain.res.herv$display <- gene_table[rownames(brain.res.herv),]$display
+brain.res.herv$class <- gene_table[rownames(brain.res.herv),]$gene_type
+
+sig.hervs <- subset(brain.res.herv, padj < pval & abs(log2FoldChange) > lfc.cutoff)
+sig.hervs <- sig.hervs[order(sig.hervs$padj),]
+
+################################ LIST TOP HERVS ################################ 
+
+upvars <- rownames(subset(sig.hervs, log2FoldChange>0))
+downvars <- rownames(subset(sig.hervs, log2FoldChange<0))
+
+################################# HEATMAPS #####################################
+
+################################### SETUP ######################################
+
+cols <- rgb_gsea(palette = c("default"), n = 14, alpha = 0.7, reverse = FALSE)
+
+# Annotation column
+df <- as.data.frame(colData(brain.herv.cor.dds)[,c("SMTS","SMTSD", "SMGEBTCH")])
+df <- subset(df, select = -c(1))
+
+# Create colors for each group
+annoCol <- c(pal_jco("default", alpha = 0.8)(7)[1], pal_jco("default", alpha = 0.8)(7)[7])
+
+names(annoCol) <- unique(brain.samples$SMTSD)
+annoCol <- list(SMTSD = annoCol)
+
+
+######################### UPREGULATED IN ALL GROUPS ############################
+
+annoRow <- as.data.frame(retro.annot.v2[,c("TE_type", "Locus")])
+annoRow <- annoRow[rownames(sig.hervs),]
+annoRow <- subset(annoRow, select = -c(2))
+
+pheatmap(assay(brain.herv.cor.tform)[rownames(sig.hervs),], 
+         main="Differentially Expressed HERVs",
+         cluster_rows=TRUE,
+         show_rownames=FALSE,
+         show_colnames = FALSE,
+         color = cols,
+         scale="row",
+         breaks=seq(-3,3,length.out=14),
+         labels_row = gene_table[rownames(sig.hervs),]$display,
+         cluster_cols=TRUE, 
+         treeheight_row=0,
+         annotation_col=df,
+         annotation_row=annoRow,
+         annotation_colors = annoCol)
+
+
+############################# VOLCANO DZ VS LZ #################################
+
+EnhancedVolcano(sig.hervs,
+                lab = rownames(sig.hervs),
+                x = 'log2FoldChange',
+                y = 'pvalue',
+                title = 'Frontal Cortex vs Hippocampus')
+
+
+
+########################## PLOT INDIVIDUAL HERVs ###############################
+
+plot.counts <- function(df, gene) {
+  
+  as.data.frame(plotCounts(df, 
+                           gene=gene, 
+                           intgroup="SMTSD", 
+                           returnData = TRUE)) %>%
+    ggplot(aes(x=SMTSD, y=count, fill=SMTSD))  +
+    geom_boxplot() +
+    theme_pubr() +
+    theme(legend.position="none",
+          axis.text.x = element_text(angle=45, hjust=1)) +
+    xlab("Cell Type") +
+    ylab("Counts") +
+    scale_x_discrete(labels=c("Brain - Frontal Cortex (BA9)" = "FC", 
+                              "Brain - Hippocampus" = "HC")) +
+    scale_fill_manual(values = c("Brain - Frontal Cortex (BA9)" = pal_jco("default", alpha = 0.8)(7)[1],
+                                 "Brain - Hippocampus" = pal_jco("default", alpha = 0.8)(7)[7])) + 
+    scale_y_continuous(expand = c(0, 0), limits = c(0, NA)) +
+    ggtitle(gene) + 
+    scale_y_log10(labels = label_comma()) +
+    theme(plot.title = element_text(hjust = 0.5),
+          aspect.ratio = 1) +
+    stat_compare_means(comparisons = list(c("Brain - Frontal Cortex (BA9)", "Brain - Hippocampus")),
+                       method = "t.test", 
+                       label = "p.signif",
+                       hide.ns = FALSE)
+}
+
+
+
+plot.counts(brain.herv.cor.dds, "HERVH_12q13.2b")
+plot.counts(brain.herv.cor.dds, "ERVLB4_20q13.12a")
+
+
 
